6e88f3b657dc854c00e39645b147418d9829fede
Samim Aktar <samimaktar94320@gmail.com>
2024-01-23 23:22:55 +0530

    updated

--- src/App.jsx
+++ src/App.jsx
@@ -8,10 +8,17 @@ function App() {
   const [todos, setTodos] = useState([]);
 
   return (
-    <div className='bg-indigo-400 h-screen w-full flex items-center justify-center' data-testid='flex-div'>
+    <div
+      className='bg-indigo-400 h-screen w-full flex items-center justify-center'
+      data-testid='flex-div'
+    >
       <div className='w-[90%] md:w-1/2  xl:w-2/5 bg-white shadow-xl rounded-md p-4'>
         <Input setTodos={setTodos} />
-        {/* Write your code here */}
+        <div className='max-h-72 min-h-20 overflow-y-auto mt-5'>
+          {todos.map((todo) => (
+            <TodoItem key={todo.id} todo={todo} setTodos={setTodos} />
+          ))}
+        </div>
       </div>
     </div>
   );
